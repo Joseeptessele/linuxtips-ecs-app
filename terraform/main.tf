056d06cb32d74371cf43002b978f8f42cd724659
Joseeptessele <josetesselep@gmail.com>
2024-10-16 23:46:29 -0300

    add efs configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "service" {
-  source                      = "github.com/Joseeptessele/linuxtips-ecs-service-module?ref=v1"
+  source                      = "github.com/Joseeptessele/linuxtips-ecs-service-module?ref=v1.1.0"
   region                      = var.region
   cluster_name                = var.cluster_name
   service_name                = var.service_name
@@ -23,6 +23,16 @@ module "service" {
     data.aws_ssm_parameter.private_subnet_a.value,
     data.aws_ssm_parameter.private_subnet_b.value,
     data.aws_ssm_parameter.private_subnet_c.value,
+  ]
+
+  efs_volumes = [
+    {
+      volume_name      = "volume-de-exemplo"
+      file_system_id   = aws_efs_file_system.main.id
+      file_system_root = "/"
+      mount_point      = "/mnt/efs"
+      read_only        = false
+    }
   ]
 
   # Autoscaling
