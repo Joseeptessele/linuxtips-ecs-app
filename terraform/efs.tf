f411f07107d837db733b50af485e400a6ec6ecca
Joseeptessele <josetesselep@gmail.com>
2024-10-16 23:47:48 -0300

    add efs configuration

---
+++ terraform/efs.tf
@@ -0,0 +1,49 @@
+resource "aws_efs_file_system" "main" {
+  creation_token   = format("%s-efs", var.service_name)
+  performance_mode = "generalPurpose"
+}
+
+resource "aws_security_group" "efs" {
+  name        = "efs_sg"
+  description = format("%s-efs-sg", var.service_name)
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# must be created according to the number of subnets
+resource "aws_efs_mount_target" "mount_a" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_a.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
+
+resource "aws_efs_mount_target" "mount_b" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_b.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
+
+resource "aws_efs_mount_target" "mount_c" {
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id      = data.aws_ssm_parameter.private_subnet_c.value
+  security_groups = [
+    aws_security_group.efs.id
+  ]
+}
